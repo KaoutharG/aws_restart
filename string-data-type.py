f408c4a8d9fdeed119d6597cc63fcac9396e52db
KaoutharG <100047896+KaoutharG@users.noreply.github.com>
2022-02-19 22:40:09 +0100

    Add files via upload

---
+++ string-data-type.py
@@ -0,0 +1,13 @@
+myString = "This is a String."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name =input( "What is your name? ")
+print(name)
+Color = input("What is your favorite color? ")
+Animal = input("What is your favorite animal? ")
+print("{}, you like a {} {}!".format(name,Color,Animal))
